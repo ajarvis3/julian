1d33925a111039eb9a02a219d3dc302a90f44fbf
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2022-08-15 16:36:52 -0400

    Mostly Finished, also initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Julian from "./julian/Julian";
+import DatePickerWrapper from "./julian/DatePickerWrapper";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [currDate, setDate] = useState(new Date());
+   return (
+      <div className="App">
+         <header className="App-header">
+            <DatePickerWrapper currDate={currDate} setDate={setDate} />
+            <Julian date={currDate} />
+         </header>
+      </div>
+   );
 }
 
 export default App;
